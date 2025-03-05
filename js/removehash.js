615fb54558ff6561e2cbe12d1e56ce422d788c99
cronozx <cronozx00@gmail.com>
2025-03-05 12:35:49 -0500

    made changes to almost every bit of the code finished the basis for the skills tab and reformatted css to look nicer

--- js/removehash.js
+++ js/removehash.js
@@ -1,19 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const link = document.querySelector('.nav-item .nav-link[href="#aboutme"]');
-
-    if (link) {
-        link.addEventListener("click", function(event) {
-            event.preventDefault();
-            const targetId = this.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
-
-            if (targetElement) {
-                targetElement.scrollIntoView({ behavior: 'smooth' });
-
-                setTimeout(function() {
-                    history.replaceState(null, null, ' ');
-                }, 1000);
-            }
-        });
-    }
+    history.pushState("", document.title, window.location.pathname);
 });
